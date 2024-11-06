67835eb1e4c38640d0f9cfc4885c83cfd96aac63
mayrars <mayra203@gmail.com>
2024-11-06 06:52:33 -0600

    Created Page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-center my-24">
+        <h1 class="text-4xl font-bold tracking-widest text-red-500">Countries</h1>
+    </div>
+</template>
+<script setup>
+</script>
