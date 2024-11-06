a23c88c744fcaa166e542e1d948f81cbe0ad7337
mayrars <mayra203@gmail.com>
2024-11-06 06:52:02 -0600

    Create and impor countryList

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts">
+<script setup>
+import { onMounted, ref } from 'vue';
+import PageHeader from './components/PageHeader.vue';
+import CountryList from './components/CountryList.vue';
+import axiosClient from './utils/axios';
+
+
+const countries = ref([])
+
+const fetchCountries = async ()=>{
+  try{
+    const {data} = await axiosClient.get("/all");
+    countries.value = data
+  }catch(error){
+    console.log(error)
+  }
+}
+onMounted(()=>{
+  fetchCountries()
+})
 </script>
 
 <template>
+  <PageHeader />
+  <div class="container max-w-screen-lg mx-auto px-6">
+    <CountryList :countries="countries"/>
+  </div>
 </template>
---
+++ src/components/CountryList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { Country } from '../models/country.model';
+
+interface Props{
+    countries: Country[];
+}
+
+defineProps<Props>()
+</script>
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        <div v-for="country in countries" class=" border border-gray-300 rounded p-3">
+            {{ country.name.common }}
+        </div>
+    </div>
+</template>
