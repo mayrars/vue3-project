1c917facfcd05fbad3785795e4cced8afba4f838
mayrars <mayra203@gmail.com>
2024-11-11 07:13:05 -0600

    created pagination

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import PageHeader from './components/PageHeader.vue';
 import CountryList from './components/CountryList.vue';
 import axiosClient from './utils/axios';
@@ -7,6 +7,11 @@ import axiosClient from './utils/axios';
 const countries = ref([])
 const search = ref('')
 const filteredCountries = ref([])
+const page = ref(1)
+const itemsPerPage = ref(12)
+const paginatedCountries = ref([])
+const totalItems = ref(0)
+
 
 const fetchCountries = async ()=>{
   try{
@@ -20,8 +25,20 @@ const fetchCountries = async ()=>{
 const filterCountries = ()=>{
   filteredCountries.value = countries.value.filter(country => country.name.common.toLowerCase().includes(search.value.toLowerCase()))
 }
+
+const sliceCountries = (currentCountries) => {
+  const start = (page.value - 1) * itemsPerPage.value;
+  const end = page.value * itemsPerPage.value;
+  paginatedCountries.value = currentCountries.slice(start, end);
+};
+
 onMounted(()=>{
   fetchCountries()
+  sliceCountries(countries.value)
+})
+
+watch([countries],()=>{
+  sliceCountries(countries.value)
 })
 </script>
 
@@ -37,6 +54,10 @@ onMounted(()=>{
         @input="filterCountries"
       />
     </div>
-    <CountryList :countries="filteredCountries.length >0 ? filteredCountries : countries"/>
+    <div class="mb-8 flex justify-center space-x-6">
+      <button class="border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-200">Previous</button>
+      <button class="border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-200">Next</button>
+    </div>
+    <CountryList :countries="paginatedCountries"/>
   </div>
 </template>
