4bcce8145544a8dadf3169532fa47609050d249a
mayrars <mayra203@gmail.com>
2024-11-13 07:16:48 -0600

    change style

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -11,7 +11,7 @@ defineProps<Props>()
 </script>
 <template>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        <div v-for="country in countries" class=" border border-gray-300 rounded p-3">
+        <div v-for="country in countries" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-50 dark:border-gray-200">
             <CountryItem :country="country"/>
         </div>
     </div>
