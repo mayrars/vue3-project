759cd3a1acdab9c465e78a9f44ea84f29cc6c71f
mayrars <mayra203@gmail.com>
2024-11-14 06:48:41 -0600

    created new view for countries

---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue';
+import CountryList from '../components/CountryList.vue';
+import axiosClient from '../utils/axios';
+
+const countries = ref([])
+const search = ref('')
+const filteredCountries = ref([])
+const page = ref(1)
+const itemsPerPage = ref(12)
+const paginatedCountries = ref([])
+const totalItems = ref(0)
+
+
+const fetchCountries = async ()=>{
+  try{
+    const {data} = await axiosClient.get("/all");
+    countries.value = data
+    totalItems.value = countries.value.length
+  }catch(error){
+    console.log(error)
+  }
+}
+
+const filterCountries = ()=>{
+  filteredCountries.value = countries.value.filter(country => country.name.common.toLowerCase().includes(search.value.toLowerCase()))
+}
+
+const sliceCountries = (currentCountries) => {
+  const start = (page.value - 1) * itemsPerPage.value;
+  const end = page.value * itemsPerPage.value;
+  paginatedCountries.value = currentCountries.slice(start, end);
+};
+
+onMounted(()=>{
+  fetchCountries()
+})
+
+const changePage = (newPage) =>{
+  page.value = newPage
+}
+
+watch([countries,page, filteredCountries],()=>{
+  sliceCountries(
+    filteredCountries.value.length <= 0 &&  search.value==="" ? countries.value : filteredCountries.value
+  )
+})
+</script>
+<template>
+    <div>
+        <div class="mb-8">
+            <input 
+                type="text" 
+                class="border border-gray-300 rounded-md px-4 py-2 w-full" 
+                placeholder="Search by country name..."
+                v-model="search"
+                @input="filterCountries"
+            />
+        </div>
+        <div class="mb-8 flex justify-center space-x-6">
+            <button @click="$event=>changePage(page - 1)" :disabled="page <= 1" :class="{'opacity-50 cursor-not-allowed': page <= 1}" class="border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-200">Previous</button>
+            <button @click="$event=>changePage(page + 1)" :disabled="page >= totalItems / itemsPerPage" :class="{'opacity-50 cursor-not-allowed': page >= totalItems / itemsPerPage}"  class="border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-200">Next</button>
+        </div>
+        <CountryList :countries="paginatedCountries"/>    
+    </div>
+</template>
