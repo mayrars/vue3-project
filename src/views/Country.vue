776a8793cd5fd9b41c9edf28c9eddc9870c9223f
mayrars <mayra203@gmail.com>
2024-11-14 06:50:02 -0600

    Get country by name and showed elements

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,8 +1,33 @@
-<script setup>
-
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    import axiosClient from '../utils/axios';
+    import { useRoute } from 'vue-router';
+    import { Country } from '../models/country.model';
+    const route = useRoute();
+    const country = ref<Country>();
+    const fetchCountries = async ()=>{
+        try {
+            const {data} = await axiosClient.get(`/name/${route.params.name}`);
+            country.value = data[0]
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    onMounted(()=>{
+        fetchCountries()
+    })
 </script>
 <template>
     <div>
-        <h1>Country</h1>
+        <h1 class="text-2xl font-bold text-center">{{country?.name.common}}</h1>
+        <div class="flex flex-col md:flex-row justify-center items-center gap-10">
+            <img :src="country?.flags.png" :alt="country?.name.common" class="w-1/2 md:w-1/4">
+            <div class="flex flex-col gap-5">
+                <p class="text-lg font-bold">Population: {{country?.population}}</p>
+                <p class="text-lg font-bold">Region: {{country?.region}}</p>
+                <p class="text-lg font-bold">Capital: {{country?.capital ? country?.capital[0] : 'N/A'}}</p>
+                <p class="text-lg font-bold">Languages: {{country?.languages ? Object.values(country?.languages).join(', ') : 'N/A'}}</p>
+            </div>
+        </div>
     </div>
 </template>
