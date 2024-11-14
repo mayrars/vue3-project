45ac4f81a7a7b1673a08ff663b615778f1a9ac92
mayrars <mayra203@gmail.com>
2024-11-14 06:49:34 -0600

    added button and  userouter

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
 import { Country } from '../models/country.model';
+
+const router = useRouter();
 
 interface Props{
     country: Country
@@ -22,11 +25,11 @@ defineProps<Props>()
         </p>
         <p class="text-sm text-gray-400">Population: <span class="font-bold">{{  country.population?.toLocaleString()  }}</span></p>
         <p class="text-sm text-gray-400">Region: <span class="font-bold">{{  country.region  }}</span></p>
-        <a href="#" class="mt-8 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button class="mt-8 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="router.push(`/country/${country.name.common}`)">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </button>
     </div>
 </template>
