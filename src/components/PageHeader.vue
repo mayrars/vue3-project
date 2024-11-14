124e9174aac42c200ec838dd5a04a95dd14aae3e
mayrars <mayra203@gmail.com>
2024-11-14 06:49:05 -0600

    change margin

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center my-24">
+    <div class="text-center mt-24 mb-5">
         <h1 class="text-4xl font-bold tracking-widest text-red-500">Countries</h1>
     </div>
 </template>
