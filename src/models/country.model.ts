6b489a2d42c719f08bd1c9c0e88dc26804880aa8
mayrars <mayra203@gmail.com>
2024-11-15 07:10:34 -0600

    add more properties to country

--- src/models/country.model.ts
+++ src/models/country.model.ts
@@ -1,13 +1,18 @@
 export interface Country {
     name: {
         common: string;
+        official: string;
     };
     capital: string[];
     cca3: string;
     flags: {
         alt: string;
         png: string;
+        svg: string;
     };
     population: number;
     region: string;
+    currencies: string[];
+    languages: string[];
+    
 }
