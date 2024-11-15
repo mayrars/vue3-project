838b1ed7f36ad0bdabfe0fd4f1924da10ac12677
mayrars <mayra203@gmail.com>
2024-11-15 07:11:06 -0600

    change size container

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
   <PageHeader />
-  <div class="container max-w-screen-lg mx-auto px-6">
+  <div class="container mx-auto px-6">
     <router-view></router-view>
   </div>
 </template>
